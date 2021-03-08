0a9e7fce620b27897ff51292efdbbcf1271380a5
Salman Ahmed <salmanahmed4858@gmail.com>
2021-03-08 22:47:49 +0500

    revert changes.

--- src/custom-pan.js
+++ src/custom-pan.js
@@ -64,8 +64,7 @@ class CustomPan extends FabricCanvasTool {
 
       let zoom = canvas.getZoom();
       console.log("ZOOM ", zoom, zoomOpts, delta);
-      // if (delta > zoom) delta += zoom;
-      zoom = (zoom * delta) / zoomOpts.zoomStep;
+      zoom = (zoom - delta) / zoomOpts.zoomStep;
       if (zoom > zoomOpts.maxZoom) zoom = zoomOpts.maxZoom;
       if (zoom < zoomOpts.minZoom) zoom = zoomOpts.minZoom;
       canvas.zoomToPoint({ x: p1.clientX, y: p1.clientY }, zoom);
