0db8b23d2bad6666ee8c6db36e353c5c263e55ed
Salman Ahmed <salmanahmed4858@gmail.com>
2021-03-08 22:13:06 +0500

    Change delta value.

--- src/custom-pan.js
+++ src/custom-pan.js
@@ -63,12 +63,11 @@ class CustomPan extends FabricCanvasTool {
       const p2 = e.e.touches[1];
 
       let zoom = canvas.getZoom();
-
-      zoom = zoom - delta / zoomOpts.zoomStep;
       console.log("ZOOM ", zoom, zoomOpts, delta);
+      zoom = zoom - delta / zoomOpts.zoomStep;
       if (zoom > zoomOpts.maxZoom) zoom = zoomOpts.maxZoom;
       if (zoom < zoomOpts.minZoom) zoom = zoomOpts.minZoom;
-      canvas.zoomToPoint({ x: p1.clientX, y: p1.clientY }, zoom);
+      canvas.zoomToPoint({ x: p1.clientX, y: p1.clientY }, delta);
 
       // let zoomStartScale = canvas.getZoom();
       // let zoom = e.self.scale;
